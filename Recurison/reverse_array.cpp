b563fd87b14adf5c7410fb9b69b0d0cae53910e4
Chinmay Deo <chinmaydeo2004@gmail.com>
2023-08-30 15:20:17 +0530

    corrected the stupidest mistake of my life

--- Recurison/reverse_array.cpp
+++ Recurison/reverse_array.cpp
@@ -7,19 +7,19 @@ bool reverse(string name, int n, int i)
 {
     if(i<=n/2)
     {
-        if(name[i] != name[n-i-1]);
+        if(name[i] != name[n-i-1])
             return false;
+        reverse(name, n, i+1);
     }
-    reverse(name, n, i+1);
         return true;
 
 }
 
 int main()
 {
-    string name = "mommy";
+    string name = "swaha";
 
-    cout<<reverse(name, name.length()-1, 0);
+    cout<<reverse(name, name.length(), 0);
     return 0;
 
 }
